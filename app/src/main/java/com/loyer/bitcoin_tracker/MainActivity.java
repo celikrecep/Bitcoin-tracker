1894983359f411d430da19531361c53a2624e87c
celikrecep <recep_clk10@hotmail.com>
2018-02-09 12:44:50 +0300

    ver 1

---
+++ app/src/main/java/com/loyer/bitcoin_tracker/MainActivity.java
@@ -0,0 +1,40 @@
+package com.loyer.bitcoin_tracker;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private final String BASE_URL = "https://apiv2.bitcoin ...";
+
+
+    TextView mPriceTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPriceTextView = (TextView) findViewById(R.id.priceLabel);
+        Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.currency_array, R.layout.spinner_item);
+
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
+
+    }
+
+
+
+
+}
